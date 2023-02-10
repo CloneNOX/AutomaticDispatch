eb8aeb3b4fd01ec123f4fad92ac2b3655ae25968
CloneNOX <2511872384@qq.com>
2023-02-10 11:25:56 +0800

    更改conda_env的内容，直接使用FastText的官方接口

--- conda_env.sh
+++ conda_env.sh
@@ -1,7 +1,6 @@
 # 请确保先进入了工作的conda环境，使用--yes以自动确认
-conda create -n pt-py3.9 python=3.9 --yes
+conda create -n fasttext python=3.9 --yes
 conda install numpy --yes
 conda install tqdm --yes
 conda install pandas --yes
-conda install pytorch torchvision torchaudio pytorch-cuda=11.6 -c pytorch -c nvidia --yes
-pip install jieba
+pip install fasttext
