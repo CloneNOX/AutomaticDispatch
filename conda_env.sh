b8eff8520f6760ff90bd9d76effa81c1882c163f
CloneNOX <2511872384@qq.com>
2023-02-16 15:48:49 +0800

    通过调整学习率，提升了模型性能

--- conda_env.sh
+++ conda_env.sh
@@ -1,7 +1,10 @@
 # 请确保先进入了工作的conda环境，使用--yes以自动确认
 conda create -n fasttext python=3.9 --yes
+conda activate fasttext
 conda install numpy --yes
 conda install tqdm --yes
 conda install pandas --yes
 conda install scipy --yes
+conda install flask --yes
 pip install fasttext
+pip install jieba
