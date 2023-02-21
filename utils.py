bb597ba12f7c49e3b12893f5b202b4199263aee5
CloneNOX <2511872384@qq.com>
2023-02-21 14:38:23 +0800

    在test.ipynb中实现了在旧数据集上的测试

--- utils.py
+++ utils.py
@@ -3,6 +3,6 @@ import jieba
 
 # 整理
 def fixText(text):
-    text = re.sub('[^\u4e00-\u9fa5]+', '', text)
+    text = re.sub(r'[^\u4e00-\u9fa5]+', '', text)
     text = ' '.join(jieba.lcut(text))
-    return text
+    
