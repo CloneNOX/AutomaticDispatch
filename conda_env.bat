6e5c703abe521d52d237604b2af1cbf673dc89e2
CloneNOX <2511872384@qq.com>
2023-03-27 10:48:41 +0800

    调整conda环境安装脚本

--- conda_env.bat
+++
@@ -1,6 +0,0 @@
-call conda create -n fasttext python=3.9 --yes
-call conda install numpy --yes
-call conda install tqdm --yes
-call conda install pandas --yes
-call conda install scipy --yes
-call pip install fasttext
--- conda_env.sh
+++ conda_env.sh
@@ -1,11 +1,2 @@
 # 请确保先进入了工作的conda环境，使用--yes以自动确认
-conda create -n fasttext python=3.9 --yes
-conda activate fasttext
-conda install numpy --yes
-conda install tqdm --yes
-conda install pandas --yes
-conda install scipy --yes
-conda install flask --yes
-conda install xlrd --yes
-pip install fasttext
-pip install jieba
+conda create -n paddle python=3.8.8 --yes
